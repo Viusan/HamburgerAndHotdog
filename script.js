500b0b77bd1080c8c6265fd375dc29ad0106035d
ViusanSantos <viusansantos@gmail.com>
2023-03-13 13:21:34 +0100

    90 PERCENT

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var dropDownEL = document.getElementById('fooddropdown');
 var foodDivEl = document.getElementById('fooddiv');
+var checkboxEl = document.getElementById('checkbox');
+var sliderEl = document.getElementById('slider');
+
+var mustardText = document.getElementById('mustardField');
+
 dropDownEL.addEventListener('change', dropDownSelected);
+sliderEl.addEventListener('change', sliderSelected);
 
 function dropDownSelected(e) {
   console.log(e.target.value);
@@ -18,9 +24,22 @@ function dropDownSelected(e) {
     <input type="radio" name="foodchoice" id="choice1">
     <label for="choice1">Hotdog Bread</label>
     <input type="radio" name="foodchoice" id="choice2">
-    <label for="choice2">Mustard</label>
+    <label for="choice2">BBQ Sauce</label>
     <input type="radio" name="foodchoice" id="choice3">
     <label for="choice3">Ketchup</label>
     `
   }
+}
+
+function sliderFunction() {
+  if(checkboxEl.checked == true) {
+    sliderEl.disabled = false;
+  }else{
+    sliderEl.disabled = true;
+    mustardText.innerHTML = "";
+  }
+}
+
+function sliderSelected() {
+  mustardText.innerHTML = sliderEl.value/10 + " gram mustard added";
 }
